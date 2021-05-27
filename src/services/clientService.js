b616097565189b6e1db09b9a215c0c8862376885
Patrick Oryono <patricken08@gmail.com>
2021-05-27 17:24:06 +0300

    minor change

--- src/services/clientService.js
+++ src/services/clientService.js
@@ -3,11 +3,3 @@ import client from "../client";
 export function getClients() {
     return client.get('/clients')
 }
-
-export function getClient(clientId) {
-    return client.get(`/clients/${clientId}`)
-}
-
-export function createClient(form) {
-    return client.post('/clients', form, {})
-}
