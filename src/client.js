93be81ef9bd1376da7fcf3aaeaad2c040d03f0ce
Patrick Oryono <patricken08@gmail.com>
2021-05-27 15:12:11 +0300

    remove timeout

--- src/client.js
+++ src/client.js
@@ -1,11 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const token = localStorage.getItem('credentials') ? JSON.parse(localStorage.getItem('credentials')).access_token : null
+const token = localStorage.getItem('credentials')
+  ? JSON.parse(localStorage.getItem('credentials')).access_token
+  : null;
 
 export default axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-    timeout: 1000,
-    headers: {
-        Authorization: `Bearer ${token ? token : null}`
-    }
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${token ? token : null}`,
+  },
 });
